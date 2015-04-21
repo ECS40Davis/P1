6ec3cc5d44828f5e1ea810eb7f5e74f4dadc958d
MaJessica9 <jamma@ucdavis.edu>
2015-04-21 14:42:29 -0700

    Update vector.h

--- vector.h
+++ vector.h
@@ -1,28 +1,35 @@
+//Style: 0 out of 9
+//Design: 16 out of 23
+//Const: 1 out of 7
+//Operation: 11 out of 11
+
 #ifndef VECTOR_H
-#define VECTOR_H
+  #define VECTOR_H
 #include "city.h"
 
 class Vector
 {
-    public:
-    
+      public:
+
         City *cityArray;
         int size;
         int count;
     
+      
+
         // sets default values
         void initialize();
     
-        // double array size if necessary
+        // double array siz if necessary
         void resize();
     
         // reads airportsLL.txt, calls City's readAirport()
         void readAirports();
     
-        // read line from citypopulations.csv + store data in cityArray
+        // read line from citypopulations.csv + store data in cityarray
         void readCities();
         
-        // return city index in cityArray that has specified airport, else -1
+        // return city index in cityarray that has specified airport, else -1
         int findAirport(char *airportName);
         
         void cleanCities();
