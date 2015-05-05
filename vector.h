c8886ecbf7766431dd30a0664c7215f7676f186e
MaJessica9 <jamma@ucdavis.edu>
2015-05-04 19:20:53 -0700

    Update vector.h

--- vector.h
+++ vector.h
@@ -1,35 +1,44 @@
 #ifndef VECTOR_H
-#define	VECTOR_H
-// Author Sean Davis
-
+  #define VECTOR_H
 #include "city.h"
-#include "plane.h"
 
 class Vector
 {
-  // City Declaration
-  City *cityArray;
-  int size;
-  int count;
-  void resize();
-  
-  // Plane Declaration
-  Plane *planeArray;
-  
-public:
-  Vector();
-  ~Vector();
-  // For City
-  void calcAirportTraffic(int index) const;
-  void calcDistance(int index1, int index2) const;
-  void cleanCities();
-  int findAirport(const char *airport) const;
-  void readAirports();
-  void readCities();
-  
-  // For Plane
-  int readPlanes();
-};  // class Vector
+      public:
 
-#endif	// VECTOR_H 
+        City *cityArray;
+        int size;
+        int count;
+    
+      
 
+        // sets default values
+        void initialize();
+    
+        // double array siz if necessary
+        void resize();
+    
+        // reads airportsLL.txt, calls City's readAirport()
+        void readAirports();
+    
+        // read line from citypopulations.csv + store data in cityarray
+        void readCities();
+        
+        // return city index in cityarray that has specified airport, else -1
+        int findAirport(const char *airportName, const City *cityArr);
+        
+        void cleanCities();
+    
+        // calls City's calcDistance()
+        void calcDistance(const Vector * vect, char *abv1, char *abv2);
+    
+        // kills memory leaks or unrecoverable space
+        void deallocate();
+        
+        char *returnname(char *abv1);
+        
+        char *returnStateName(char *abv1);
+        
+}; //endclass
+                      
+#endif
